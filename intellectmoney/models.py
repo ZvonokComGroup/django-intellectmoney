7a6c9d736b3b54bc747ab49f46cdabe869cbf9dc
Ivan Petuhov <ivan@ostrovok.ru>
2017-08-18 11:56:44 +0300

    fix

--- intellectmoney/models.py
+++ intellectmoney/models.py
@@ -6,3 +6,6 @@ class IntellectMoney(models.Model):
 
     created = models.DateTimeField(auto_now=True)
     orderId = models.CharField(unique=True, editable=False, max_length=255)
+
+    def __str__(self):
+        return 'IntellectMoney payment (orderId: {})'.format(self.orderId)

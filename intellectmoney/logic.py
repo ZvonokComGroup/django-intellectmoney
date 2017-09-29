82fb3db11a454e84cd908d3017ac88a0fc764f48
Ivan Petuhov <ivan@ostrovok.ru>
2017-09-29 13:55:48 +0300

    add tax nds options

--- intellectmoney/logic.py
+++ intellectmoney/logic.py
@@ -8,6 +8,14 @@ CONTENT_TYPE_EXPENSES = 3
 CONTENT_TYPE_RETURN_EXPENSES = 4
 
 
+TAX_NDS_18 = 1  # ставка НДС 18%
+TAX_NDS_10 = 2  # ставка НДС 10%
+TAX_NDS_18_118 = 3  # ставка НДС расч. 18/118
+TAX_NDS_10_110 = 4  # ставка НДС расч. 10/110
+TAX_NDS_0 = 5  # ставка НДС 0%
+TAX_NO_NDS = 6  # НДС не облагается
+
+
 def getMerchantReceiptPosition(quantity, price, tax, text):
     return {
         'quantity': quantity,
